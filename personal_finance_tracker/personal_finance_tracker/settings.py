da7a99c7958db859d3f109c7d10bf428dc69f26d
Ethan Ondzik <ethan.ondzik@gmail.com>
2025-02-26 11:59:30 -0800

    Added basic dashboard page with corrosponding view, url, and static files, and added finance tracker to installed apps in settings.py.

--- personal_finance_tracker/personal_finance_tracker/settings.py
+++ personal_finance_tracker/personal_finance_tracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finance_tracker',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'personal_finance_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
