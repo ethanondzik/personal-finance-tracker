83e785bed6626283a59c9cfee3380abf8188d85e
Ethan Ondzik <ethan.ondzik@gmail.com>
2025-05-07 23:04:20 -0700

    Change theme middle ware to only apply for authenticated users. Created a new authenticated_base.html template, so that there is now a seperate base template for auethnicated pages and unauthenticated pages. Updated all html pages to use these updated templates.

--- personal_finance_tracker/finance_tracker/middleware.py
+++ personal_finance_tracker/finance_tracker/middleware.py
@@ -9,24 +9,23 @@ class ThemeMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # First check for users saved theme preferences in the database
+        # Only apply theme to pages that authenticated users can access (i.e. all pages except the login and landing page)
         if request.user.is_authenticated:
             theme = request.user.theme
+
+            request.theme = theme
+            
+            # Add theme to request object for use in templates
+            request.theme = theme
+
+            # Add navbar-specific classes based on theme (to override navbar styling complexities within star admin 2)
+            if theme == 'dark':
+                request.navbar_class = 'navbar-dark'
+            else:
+                request.navbar_class = 'navbar-light'
         else:
-            # If user is not authenticated, check for a theme in cookies
-            theme = request.COOKIES.get('theme')
-
-            # If no theme cookie is found, default to light theme
-            if not theme:
-                theme = 'light'
-
-        # Add theme to request object for use in templates
-        request.theme = theme
-
-        # Add navbar-specific classes based on theme (to override navbar styling complexities within star admin 2)
-        if theme == 'dark':
-            request.navbar_class = 'navbar-dark'
-        else:
+            # Default theme for unauthenticated users
+            request.theme = 'light'
             request.navbar_class = 'navbar-light'
 
 
